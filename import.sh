4aa0babd43ad9f159bcb5ae4cc3db4a4702ad3f1
Leandro Andersen <leandro.andersen@adroll.com>
2021-05-02 22:20:45 +0100

    added some paralellization to the import process

--- import.sh
+++ import.sh
@@ -27,6 +27,7 @@ for i in $filenames
 done
 
 #script start
+N=16 #number of simultaneous processes
 ls ./inputbreach |$SORT| sed 's/^/.\/inputbreach\//' | while read inputfile;
 do
 echo "[*] Checking breach $inputfile checksum..." | $TEE -a debug.log
@@ -35,8 +36,10 @@ shasum="$(sha256sum "$inputfile" | $CUT -d' ' -f1)"
 		cat imported.log | grep "$shasum" | $TEE -a debug.log
 
 		# importing to data folder
-		find data -not -path '*/\.*' | $SORT | while read path;
+		find data -not -path '*/\.*' | $SORT | (while read path
 		do
+			((i=i%N)); ((i++==0)) && wait
+			(
 			if [ -f $path ]; then
 				filter=$(echo "$path" | sed 's/^.*data//' | sed 's#/##g' | sed 's/symbols$//g' )
 				if [ "$(echo "$path" | grep "symbols$")" == "" ]; then
@@ -47,9 +50,8 @@ shasum="$(sha256sum "$inputfile" | $CUT -d' ' -f1)"
 					grep -ai "^$filter\w*\b" "$inputfile" | grep -aiv "^$filter[a-zA-Z0-9]\w*\b" | LC_CTYPE=C sed 's/\r//g' | grep -a @ >> $path | $TEE -a debug.log
 				fi
 			fi
-		done
-
-
+			) & #parallelization - each data file will spawn a separate process
+		done && wait) #for all imports to be done
 
 		echo "[*] Logging sha256sum into imported.log..." | $TEE -a debug.log
 		echo "$($DATE --rfc-3339=date): $(sha256sum "$inputfile")	$($DU -h "$inputfile"|$CUT -d'	' -f1)" | $TEE -a imported.log | $TEE -a debug.log
