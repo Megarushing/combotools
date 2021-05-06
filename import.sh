67c3dee89c7b1ac2d609df24cb3a4d25440de61a
Leandro Andersen <leandro.andersen@adroll.com>
2021-05-06 16:15:32 +0100

    forgot the w

--- import.sh
+++ import.sh
@@ -16,7 +16,7 @@ for c in "sort" "date" "du" "tee" "cut" "wc" "uniq"
 done
 
 # bootstraps file structure for current tree
-filenames="0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v x y z symbols"
+filenames="0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z symbols"
 
 for i in $filenames
 	do for f in $(find data -type d | sed "s/$/\/$i/")
